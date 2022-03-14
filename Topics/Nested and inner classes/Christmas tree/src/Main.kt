27dcd545f271f797014e8c9b5d9380cf0a53cac4
DemonazGH <6583865@gmail.com>
2022-03-14 01:17:24 +0100

    somewhere in the middle of the project

--- Topics/Nested and inner classes/Christmas tree/src/Main.kt
+++ Topics/Nested and inner classes/Christmas tree/src/Main.kt
@@ -1,9 +1,13 @@
 class ChristmasTree(var color: String) {
 
-    // create function putTreeTopper()
+    fun putTreeTopper(color: String) {
+        val t = TreeTopper(color)
+        t.sparkle()
+    }
 
     inner class TreeTopper(var color: String) {
-
-        // create function sparkle()
+        fun sparkle() {
+            println("The sparkling $color tree topper looks stunning on the ${this@ChristmasTree.color} Christmas tree!")
+        }
     }
 }
